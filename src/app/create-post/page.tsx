d54f97a380c6d8fbd7d199f24435b731319d649d
zyycatcat <yunyanzhao5@gmail.com>
2024-12-24 17:25:28 +0800

    feat: 分页获取评论内容

--- src/app/create-post/page.tsx
+++ src/app/create-post/page.tsx
@@ -187,7 +187,7 @@ function PageContent() {
 
     return (
         <div className="container mx-auto p-4">
-            <h1 className="text-2xl font-bold mb-4">Create a New Post</h1>
+            <h1 className="text-2xl font-bold mb-4">Create New Discussion</h1>
 
             {/* 显示错误信息 */}
             {errorMessage && (
